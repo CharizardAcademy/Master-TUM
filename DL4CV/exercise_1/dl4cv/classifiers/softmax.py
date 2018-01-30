5efc4139396c7eb6c536971e1fb9ace68cd3e762
Highstars1 <gaoyingqiang@live.cn>
2018-01-30 16:37:41 +0100

    DL4CV
    DL4CV exercise

---
+++ DL4CV/exercise_1/dl4cv/classifiers/softmax.py
@@ -0,0 +1,73 @@
+"""Linear Softmax Classifier."""
+# pylint: disable=invalid-name
+import numpy as np
+
+from .linear_classifier import LinearClassifier
+
+
+def cross_entropoy_loss_naive(W, X, y, reg):
+    """
+    Cross-entropy loss function, naive implementation (with loops)
+
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
+
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
+
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    # pylint: disable=too-many-locals
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
+
+
+def cross_entropoy_loss_vectorized(W, X, y, reg):
+    """
+    Cross-entropy loss function, vectorized version.
+
+    Inputs and outputs are the same as in cross_entropoy_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
+
+
+class SoftmaxClassifier(LinearClassifier):
+    """The softmax classifier which uses the cross-entropy loss."""
+
+    def loss(self, X_batch, y_batch, reg):
+        return cross_entropoy_loss_vectorized(self.W, X_batch, y_batch, reg)
