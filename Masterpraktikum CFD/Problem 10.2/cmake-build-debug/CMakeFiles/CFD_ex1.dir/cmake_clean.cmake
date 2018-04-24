616c1f29b28b0d0bd385dbf68620b0cd9b8f266c
Highstars1 <gaoyingqiang@live.cn>
2018-04-24 18:13:03 +0200

    Add files via upload

---
+++ Masterpraktikum CFD/Problem 10.2/cmake-build-debug/CMakeFiles/CFD_ex1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CFD_ex1.dir/main.c.o"
+  "CMakeFiles/CFD_ex1.dir/helper.c.o"
+  "CMakeFiles/CFD_ex1.dir/init.c.o"
+  "CMakeFiles/CFD_ex1.dir/sor.c.o"
+  "CMakeFiles/CFD_ex1.dir/visual.c.o"
+  "CMakeFiles/CFD_ex1.dir/inple.c.o"
+  "CMakeFiles/CFD_ex1.dir/uvp.c.o"
+  "CMakeFiles/CFD_ex1.dir/boundary_val.c.o"
+  "CFD_ex1.pdb"
+  "CFD_ex1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CFD_ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
